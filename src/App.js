635e642f8dc09c11f7dfccaf9f1d09223fb33840
mpeglerg <mpeglerg@lion.lmu.edu>
2020-03-20 17:40:19 -0700

    style home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import campaignlogo from "./images/campaignlogo.png";
+import campaignposter from "./images/campaignposter.png";
+
+import AnyaTaylorHeadshot from "./images/AnyaTaylorHeadshot.JPG";
+import AnyaHeadshot from "./images/Anyaheadshot.JPG";
+//import { Document } from "react-pdf";
 import AppTabs from "./AppTabs.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -10,7 +15,7 @@ function App() {
     <div className="App">
       <Router>
         <div className="App">
-          <AppTabs></AppTabs>
+          <AppTabs className="testing" />
           <Switch>
             <Route exact path="/">
               <Home />
@@ -31,54 +36,79 @@ function App() {
 function Home() {
   return (
     <div>
-      <h1>Anya Montgomery for ASLMU President</h1>
-      <img src={campaignlogo}></img>
+      {/* <h1 className="Header">Anya Montgomery for ASLMU President</h1> */}
+      {/* <img src={campaignlogo} alt="campaignlogo" width="500px"></img> */}
+      {/* <img
+        className="AnyaTaylorHeadshot"
+        src={AnyaTaylorHeadshot}
+        alt="AnyaTaylorHeadshot"
+        width="500px"
+        //border="10px"
+        border-color="white"></img> */}
+      <img
+        className="CampaignPoster"
+        src={campaignposter}
+        alt="campaignposter"
+        width="cover"
+        style={{ marginTop: "-9%" }}></img>
     </div>
   );
 }
 
 function Campaign() {
   return (
-    <div>
-      <h2>Our Platform</h2>
-      <p>
-        We believe in our Mission: No student’s success should be hindered by
-        racial bias, mental health, gender exclusivity, inaccessibility, or food
-        insecurity. As members of the current ASLMU administration, we will use
-        our experiential knowledge, privileges, approachability, and
-        intersectional identities to center the student voice and continue
-        making LMU a healthy and exciting place to call home. If elected, we
-        will continue the dream of past and current student leaders, while
-        thinking forward and catering to the immediate needs of the LMU student
-        body.
-      </p>
-      <p>
-        We believe in Healing... There is a consistent need for opportunities to
-        heal and recharge from the impact of academics and extracurricular
-        involvements. We’ll collaborate with SPS to increase service
-        accessibility and transparency in the way of an alternative
-        appointment-booking system. We hope to provide resources for students
-        struggling to communicate with professors about their mental health
-        needs. Many students are overcommitted and often neglect de-stressing.
-        We plan to implement a cumulative point system that rewards students for
-        their ongoing attendance at sporting events and the student-organized
-        programming that brings life to the LMU experience.{" "}
-      </p>
-      <p>
-        We believe in Community... We'll continue our current hands-on work in
-        getting menstrual hygiene products in more public restrooms and
-        demanding LMU’s divestment from fossil fuel companies. We will also
-        engage with organizations such as LGBTSS to improve resources for our
-        trans students and staff. With support from grassroots student-media
-        platforms, such as LMU Agency, we will advocate for continued action on
-        these initiatives and develop the campus understanding of intersectional
-        issues that center the experiences of communities of color. We’ll
-        collaborate with departments including CSA and EIS to encourage
-        campus-wide reflection on our mission, sometimes savioristic practices,
-        and the ways that we can come together to improve our Lion community and
-        beyond.
-      </p>
-      <p>With all of us together, “the Dream lives on"</p>
+    <div className="Campaign">
+      <h2 className="PlatformTitle">Our Platform</h2>
+      <div className="MissionText">
+        <h3 className="MissionTitle">We believe in our Mission: </h3>
+        <p>
+          No student’s success should be hindered by racial bias, mental health,
+          gender exclusivity, inaccessibility, or food insecurity. As members of
+          the current ASLMU administration, we will use our experiential
+          knowledge, privileges, approachability, and intersectional identities
+          to center the student voice and continue making LMU a healthy and
+          exciting place to call home. If elected, we will continue the dream of
+          past and current student leaders, while thinking forward and catering
+          to the immediate needs of the LMU student body.
+        </p>
+      </div>
+      <div className="HealingText">
+        <h3 className="HealingTitle">We believe in Healing...</h3>
+        <p>
+          There is a consistent need for opportunities to heal and recharge from
+          the impact of academics and extracurricular involvements. We’ll
+          collaborate with SPS to increase service accessibility and
+          transparency in the way of an alternative appointment-booking system.
+          We hope to provide resources for students struggling to communicate
+          with professors about their mental health needs. Many students are
+          overcommitted and often neglect de-stressing. We plan to implement a
+          cumulative point system that rewards students for their ongoing
+          attendance at sporting events and the student-organized programming
+          that brings life to the LMU experience.
+        </p>
+      </div>
+      <div className="CommunityText">
+        <h3 className="CommunityTitle">We believe in Community... </h3>
+        <p>
+          We'll continue our current hands-on work in getting menstrual hygiene
+          products in more public restrooms and demanding LMU’s divestment from
+          fossil fuel companies. We will also engage with organizations such as
+          LGBTSS to improve resources for our trans students and staff. With
+          support from grassroots student-media platforms, such as LMU Agency,
+          we will advocate for continued action on these initiatives and develop
+          the campus understanding of intersectional issues that center the
+          experiences of communities of color. We’ll collaborate with
+          departments including CSA and EIS to encourage campus-wide reflection
+          on our mission, sometimes savioristic practices, and the ways that we
+          can come together to improve our Lion community and beyond.
+        </p>
+      </div>
+      <div className="SloganText">
+        <h3>With all of us together, “the Dream lives on"</h3>
+      </div>
+      <div className="LogoImage">
+        <img src={campaignlogo} alt="camapignlogo"></img>
+      </div>
     </div>
   );
 }
@@ -86,10 +116,22 @@ function Campaign() {
 function AboutUs() {
   return (
     <div>
+      <img
+        className="AnyaHeadshot"
+        src={AnyaHeadshot}
+        alt="AnyaHeadshot"
+        width="25%"
+      />
       <h1>About Anya:</h1>
       <p>Women’s and Gender Studies Major, African American Studies Minor</p>
       <h1>About Taylor:</h1>
       <p>Anti-racism Studies, Spanish Double Major</p>
+      <img
+        className="AnyaTaylorHeadshot"
+        src={AnyaTaylorHeadshot}
+        alt="AnyaTaylorHeadshot"
+        width="50%"
+      />
       <h2>Involvements: </h2>
       <p>
         ASLMU Secretary ASLMU NCAA Senator Women’s Swim Team Sursum Corda
--- src/AppTabs.js
+++ src/AppTabs.js
@@ -3,23 +3,44 @@ import React from "react";
 //import homeIcon from "./images/home.svg";
 import { NavLink } from "react-router-dom";
 
+//import url("fonts/FuturaBoldItalic.ttf")
+
 export default function AppTabs() {
   return (
-    <nav>
-      <NavLink activeClassName="ActiveHomeTab" exact id="HomeTab" to="/">
-        Home
-        {/* <img src={homeIcon} alt="homeIcon" width="30px" height="30px"></img> */}
-      </NavLink>
-      <NavLink activeClassName="ActiveAboutUsTab" id="AboutUsTab" to="/AboutUs">
-        About Us
-      </NavLink>
-      <NavLink
-        activeClassName="ActiveCampaignTab"
-        exact
-        id="CampaignTab"
-        to="/Campaign">
-        Campaign
-      </NavLink>
-    </nav>
+    <div className="AllTabs">
+      <nav style={{ paddingTop: "15px", paddingLeft: "15px" }}>
+        <NavLink
+          style={{ textDecoration: "none", color: "#f79051" }}
+          className="HomeTab"
+          activeClassName="ActiveHomeTab"
+          exact
+          id="HomeTab"
+          to="/">
+          <p>
+            <font face="FuturaItalicBold">Home</font>
+          </p>
+          {/* <img src={homeIcon} alt="homeIcon" width="30px" height="30px"></img> */}
+        </NavLink>
+        <NavLink
+          style={{ textDecoration: "none", color: "#f79051" }}
+          activeClassName="ActiveAboutUsTab"
+          id="AboutUsTab"
+          to="/AboutUs">
+          <p>
+            <font face="FuturaItalicBold">About Us</font>
+          </p>
+        </NavLink>
+        <NavLink
+          style={{ textDecoration: "none", color: "#f79051" }}
+          activeClassName="ActiveCampaignTab"
+          exact
+          id="CampaignTab"
+          to="/Campaign">
+          <p className="CampaignText">
+            <font face="FuturaItalicBold">Campaign</font>
+          </p>
+        </NavLink>
+      </nav>
+    </div>
   );
 }
