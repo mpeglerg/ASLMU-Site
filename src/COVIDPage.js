d267736e2e8d9efc024a0e1bc90dc9b7e72219a6
mpeglerg <mpeglerg@lion.lmu.edu>
2020-03-27 20:07:38 -0700

    edit/style covid page

--- src/COVIDPage.js
+++ src/COVIDPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./HomePage.css";
-
+import FuturaBoldItalic from "./fonts/futura/FuturaBoldItalic.ttf";
 import "./COVIDPage.css";
 
 export default function COVIDPage() {
@@ -8,72 +8,121 @@ export default function COVIDPage() {
     <div>
       <h1 className="CovidTitle">Our Response to COVID-19</h1>
       <p className="TaylorLetter">
-        Dear LMU, <br />
-        In just a short couple weeks a lot has changed. As I am sitting here at
-        my desk, with most of my friends packed up and gone, trying to write
-        this letter in response to the COVID-19 pandemic, I am recognizing that
-        I have been on autopilot for some time now. So many of my friends and
-        community have been displaced and my focus has just been trying to
-        support and assist with whatever I can. But now, as classes have started
-        and we’re all scattered around the world, my emotions are catching up to
-        me. I am overwhelmed. I imagine you all are too. But, I think it is
-        important to acknowledge what’s in our minds. We don’t always need to
-        find a solution, especially when we havn’t processed what the fuck just
-        happened. So my goal for this letter is to just feel and be. And to feel
-        and be with you all. With that, I am going to try to pinpoint some of
-        the major thoughts and feelings I have right now. Maybe acknowledging
-        what I am feeling right now can help me begin to make sense of the past
-        and move forward to the future. And if you want, you can join me on this
-        journey. <br />
-        Pressure <br />
-        First off… I feel a lot of pressure to write a nice letter that
-        identifies everything that I am and you are feeling… but I can’t do
-        that. To do so would be a disservice to me and to you because as much as
-        we are in this together, there is a lot going on and it’s not up to me,
-        nor you, to represent it all. But the pressure to do so is still there.
-        All of this extra time makes me feel like I should be getting everything
-        and more done. I can get ahead in my classes now. I can read that book
-        that people have been recommending to me. I can finally eat right,
-        hydrate, and prioritize working out. <br />
-        Tired
+        <div className="FirstParagraph">
+          Dear LMU, <br />
+          In just a short couple weeks a lot has changed. As I am sitting here
+          at my desk, with most of my friends packed up and gone, trying to
+          write this letter in response to the COVID-19 pandemic, I am
+          recognizing that I have been on autopilot for some time now. So many
+          of my friends and community have been displaced and my focus has just
+          been trying to support and assist with whatever I can. But now, as
+          classes have started and we’re all scattered around the world, my
+          emotions are catching up to me. I am overwhelmed. I imagine you all
+          are too. But, I think it is important to acknowledge what’s in our
+          minds. We don’t always need to find a solution, especially when we
+          havn’t processed what the fuck just happened. So my goal for this
+          letter is to just feel and be. And to feel and be with you all. With
+          that, I am going to try to pinpoint some of the major thoughts and
+          feelings I have right now. Maybe acknowledging what I am feeling right
+          now can help me begin to make sense of the past and move forward to
+          the future. And if you want, you can join me on this journey.
+        </div>{" "}
         <br />
-        All of that pressure to do everything makes me feel like I can’t do
-        anything. There is so much that I want to do, but now, all I feel like I
-        can do is sleep… even though my body isn’t tired. I am tired of this
-        whole situation… I have hated working endlessly to pack up my friends’
-        lives into small boxes and then say goodbye (when often many don’t even
-        know where or to what they will be going home to). All of these emails.
-        I am tired of organizing and trying to advocate to the university
-        administration about the needs of all of us students and then to only
-        realize that even though intentional policies can and are being made,
-        there will still be negative consequences--because that is just the
-        nature of what is going on right now. <br />
-        Love <br />
-        Through all of the difficulties I can’t believe how warm my heart feels
-        (ok cheesy much??). I have felt so much love towards and from students
-        and community members as we try to navigate this situation without a
-        clear roadmap. I appreciate that people have genuinely asked me how I am
-        doing during this time. Knowing that people love and care for me has
-        gotten me through (and through life in general tbh) and I can only hope
-        to reciprocate that love to you all. <br />
-        Community
-        <br /> It has been really hard not knowing what is coming next, but I
-        have felt ok getting through because I know that we have the support of
-        one and other. I am so grateful to my friends, professors, and advisors
-        who have been so accommodating and open to help during this time. It
-        means a lot (even if I don’t respond right away). It has been pretty
-        friggin cool to see the power of student mobilization. Us students at
-        LMU and across the world have come together to get our voices heard and
-        fight for what we need during this time. Whenever I feel like I am too
-        young or too dumb to do something, I will think of these past few weeks
-        and our actions to remind myself that we aren’t just capable, we’re
-        pretty good at what we do. I am inspired by you all everyday and proud
-        to be a part of the work. Niche shoutout but to all of those gyms
-        (specifically the @lmusportsperformance) who have been providing free
-        online workouts during this time… that has taken away so much of my
-        anxiety. I struggle a lot being in one spot for an extended period of
-        time and I appreciate the individuals and groups who know that feeling
-        and have worked to be there for me and others. <br />
+        <span className="SectionTitle" style={{ color: "#f79051" }}>
+          Pressure
+        </span>{" "}
+        <br />
+        <ol>
+          First off… I feel a lot of pressure to write a nice letter that
+          identifies everything that I am and you are feeling… but I can’t do
+          that. To do so would be a disservice to me and to you because as much
+          as we are in this together, there is a lot going on and it’s not up to
+          me, nor you, to represent it all. But the pressure to do so is still
+          there.
+        </ol>{" "}
+        <ol>
+          All of this extra time makes me feel like I should be getting
+          everything and more done. I can get ahead in my classes now. I can
+          read that book that people have been recommending to me. I can finally
+          eat right, hydrate, and prioritize working out.{" "}
+        </ol>
+        <br />
+        <span className="SectionTitle" style={{ color: "#f79051" }}>
+          {" "}
+          Tired{" "}
+        </span>
+        <br />
+        <ol>
+          All of that pressure to do everything makes me feel like I can’t do
+          anything. There is so much that I want to do, but now, all I feel like
+          I can do is sleep… even though my body isn’t tired.{" "}
+        </ol>{" "}
+        <ol>
+          I am tired of this whole situation… I have hated working endlessly to
+          pack up my friends’ lives into small boxes and then say goodbye (when
+          often many don’t even know where or to what they will be going home
+          to).{" "}
+        </ol>{" "}
+        <ol>
+          All of these emails. I am tired of organizing and trying to advocate
+          to the university administration about the needs of all of us students
+          and then to only realize that even though intentional policies can and
+          are being made, there will still be negative consequences--because
+          that is just the nature of what is going on right now.
+        </ol>{" "}
+        <br />
+        <span className="SectionTitle" style={{ color: "#f79051" }}>
+          Love
+        </span>{" "}
+        <br />
+        <ol>
+          Through all of the difficulties I can’t believe how warm my heart
+          feels (ok cheesy much??). I have felt so much love towards and from
+          students and community members as we try to navigate this situation
+          without a clear roadmap.{" "}
+        </ol>{" "}
+        <ol>
+          I appreciate that people have genuinely asked me how I am doing during
+          this time. Knowing that people love and care for me has gotten me
+          through (and through life in general tbh) and I can only hope to
+          reciprocate that love to you all.{" "}
+        </ol>
+        <br />
+        <span
+          className="SectionTitle"
+          style={{
+            color: "#f79051"
+          }}>
+          Community
+        </span>
+        <br />{" "}
+        <ol>
+          It has been really hard not knowing what is coming next, but I have
+          felt ok getting through because I know that we have the support of one
+          and other. I am so grateful to my friends, professors, and advisors
+          who have been so accommodating and open to help during this time. It
+          means a lot (even if I don’t respond right away).
+        </ol>{" "}
+        <ol>
+          {" "}
+          It has been pretty friggin cool to see the power of student
+          mobilization. Us students at LMU and across the world have come
+          together to get our voices heard and fight for what we need during
+          this time. Whenever I feel like I am too young or too dumb to do
+          something, I will think of these past few weeks and our actions to
+          remind myself that we aren’t just capable, we’re pretty good at what
+          we do. I am inspired by you all everyday and proud to be a part of the
+          work.{" "}
+        </ol>{" "}
+        <ol>
+          Niche shoutout but to all of those gyms (specifically the
+          @lmusportsperformance) who have been providing free online workouts
+          during this time… that has taken away so much of my anxiety. I
+          struggle a lot being in one spot for an extended period of time and I
+          appreciate the individuals and groups who know that feeling and have
+          worked to be there for me and others.{" "}
+        </ol>
+        <br />
         So those four emotions are just a small portion of what is filling my
         brain and I am not really sure what to do with them. But maybe that is
         ok. I am trying to force myself to be ok with not knowing the answers to
@@ -83,8 +132,11 @@ export default function COVIDPage() {
         do so, but through it all, we’ve still managed to be together and that
         is powerful. That will keep us going. Writing this makes me think of
         this anonymous quote: <br />
-        “Friends pick us up when we fall down, and if they can’t pick us up,
-        they lie down and listen for a while.” <br />
+        <ol>
+          “Friends pick us up when we fall down, and if they can’t pick us up,
+          they lie down and listen for a while.”
+        </ol>{" "}
+        <br />
         There is so much going on and we’re all experiencing this differently,
         with different emotions. But those emotions are real and before we can
         get the strength to get up and do, together, let’s acknowledge where we
